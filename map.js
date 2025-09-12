27142d4a5b9a26a35cc29f1388af2a3b3726e5cf
Dawid-Majcherek <112699790+Dawid-Majcherek@users.noreply.github.com>
2025-09-12 16:26:03 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('custom-map').setView([52.2297, 21.0122], 6);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
+
+    cm_data.forEach(function(item) {
+        if (item.type === "point") {
+            L.marker(item.coords).addTo(map).bindPopup(item.title);
+        } else if (item.type === "polygon") {
+            L.polygon(item.coords, { color: item.color || '#3388ff' })
+              .addTo(map)
+              .bindPopup(item.title);
+        }
+    });
+});
+
